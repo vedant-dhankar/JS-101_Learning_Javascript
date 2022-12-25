ee3a85a1da93651306bad77e7605fd68c6be4725
vedant-dhankar <vedanttdhankar@gmail.com>
2022-12-25 21:04:58 +0530

    my first assignment

---
+++ Introduction_to_Javascript/assingment1.js
@@ -0,0 +1,7 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+ 
+let a = "Masai School"
+console.log (a)
+let b = "A Transformation in Education"
+console.log (b)
